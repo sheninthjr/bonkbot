c05898790fe645ddcd9a25eb560669ba9d259c83
sheninthjr <sheninthjr23@gmail.com>
2025-02-08 21:24:56 +0530

    Bot UI updated

--- apps/telegram-backend/main.py
+++ apps/telegram-backend/main.py
@@ -17,6 +17,7 @@ load_dotenv()
 BACKEND_URL: str = 'http://localhost:3000/api/v1'
 TOKEN: str = os.getenv('TOKEN')
 
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     telegram_id = update.message.from_user.id
     response = requests.post(f'{BACKEND_URL}/user/signup', json={
@@ -28,38 +29,44 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     keyboard = [
         [
-            InlineKeyboardButton("ETH Address", callback_data=f'copy_eth_{eth_address}'),
-            InlineKeyboardButton("SOL Address", callback_data=f'copy_sol_{sol_address}')
+            InlineKeyboardButton(
+                "ETH Address", callback_data=f'copy_eth_{eth_address}'),
+            InlineKeyboardButton(
+                "SOL Address", callback_data=f'copy_sol_{sol_address}')
         ],
         [
             InlineKeyboardButton("ETH Balance", callback_data=f'eth_balance'),
             InlineKeyboardButton("SOL Balance", callback_data=f'sol_balance')
         ],
         [
-            InlineKeyboardButton("Export ETH Private Key", callback_data='export_eth_key'),
-            InlineKeyboardButton("Export SOL Private Key", callback_data='export_sol_key')
+            InlineKeyboardButton("Export ETH Private Key",
+                                 callback_data='export_eth_key'),
+            InlineKeyboardButton("Export SOL Private Key",
+                                 callback_data='export_sol_key')
         ],
         [
             InlineKeyboardButton("Transfer SOL", callback_data='sol_transfer'),
             InlineKeyboardButton("Transfer ETH", callback_data='eth_transfer')
         ],
         [
-            InlineKeyboardButton("Show Seed Phrase", callback_data='show_seedphrase')
+            InlineKeyboardButton("Show Seed Phrase",
+                                 callback_data='show_seedphrase')
         ]
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
-    
+
     await update.message.reply_text(
-        "Welcome to Jr Wallet - the fastest and most secure bot for Wallet Management", 
+        "Welcome to Jr Wallet - the fastest and most secure bot for Wallet Management",
         reply_markup=reply_markup
     )
+
 
 async def handle_button_click(update: Update, context: ContextTypes.DEFAULT_TYPE):
     query = update.callback_query
     await query.answer()
-    
+
     data = query.data
-    
+
     if data.startswith('copy_eth_'):
         eth_address = data.split('_', 2)[2]
         await context.bot.send_message(
@@ -67,7 +74,7 @@ async def handle_button_click(update: Update, context: ContextTypes.DEFAULT_TYPE
             text=f'Ethereum Address: `{eth_address}`',
             parse_mode='MarkdownV2'
         )
-    
+
     elif data.startswith('copy_sol_'):
         sol_address = data.split('_', 2)[2]
         await context.bot.send_message(
@@ -75,41 +82,45 @@ async def handle_button_click(update: Update, context: ContextTypes.DEFAULT_TYPE
             text=f'Solana Address: `{sol_address}`',
             parse_mode='MarkdownV2'
         )
-    
+
     elif data == 'export_eth_key':
         telegram_id = query.from_user.id
-        response = requests.get(f'{BACKEND_URL}/user/privatekey/ETH/?telegramId={telegram_id}')
+        response = requests.get(
+            f'{BACKEND_URL}/user/privatekey/ETH/?telegramId={telegram_id}')
         parsed_response = json.loads(response.content)
         await context.bot.send_message(
             chat_id=query.message.chat_id,
             text=f'ETH Private Key: `{parsed_response["privateKey"]}`',
             parse_mode='MarkdownV2'
         )
-    
+
     elif data == 'export_sol_key':
         telegram_id = query.from_user.id
-        response = requests.get(f'{BACKEND_URL}/user/privatekey/SOL/?telegramId={telegram_id}')
+        response = requests.get(
+            f'{BACKEND_URL}/user/privatekey/SOL/?telegramId={telegram_id}')
         parsed_response = json.loads(response.content)
         await context.bot.send_message(
             chat_id=query.message.chat_id,
             text=f'SOL Private Key: `{parsed_response["privateKey"]}`',
             parse_mode='MarkdownV2'
         )
-    
+
     elif data == 'show_seedphrase':
         telegram_id = query.from_user.id
-        response = requests.get(f'{BACKEND_URL}/user/seedphrase?telegramId={telegram_id}')
+        response = requests.get(
+            f'{BACKEND_URL}/user/seedphrase?telegramId={telegram_id}')
         parsed_response = json.loads(response.content)
         await context.bot.send_message(
             chat_id=query.message.chat_id,
             text=f'Seed Phrase: `{parsed_response["seedphrase"]}`',
             parse_mode='MarkdownV2'
         )
-    
+
     elif data == 'eth_balance':
         telegram_id = query.from_user.id
         await query.message.reply_text("Fetching...")
-        response = requests.get(f'{BACKEND_URL}/user/balance/ETH/?telegram_id={telegram_id}')
+        response = requests.get(
+            f'{BACKEND_URL}/user/balance/ETH/?telegram_id={telegram_id}')
         parsed_response = json.loads(response.content)
         ethbalance = parsed_response['ethbalance']
         await query.message.reply_text(f'Your ETH balance is: {ethbalance}')
@@ -117,7 +128,8 @@ async def handle_button_click(update: Update, context: ContextTypes.DEFAULT_TYPE
     elif data == 'sol_balance':
         telegram_id = query.from_user.id
         await query.message.reply_text("Fetching...")
-        response = requests.get(f'{BACKEND_URL}/user/balance/SOL/?telegram_id={telegram_id}')
+        response = requests.get(
+            f'{BACKEND_URL}/user/balance/SOL/?telegram_id={telegram_id}')
         print(response.content)
         parsed_response = json.loads(response.content)
         solbalance = parsed_response['solbalance']
@@ -130,6 +142,7 @@ async def handle_button_click(update: Update, context: ContextTypes.DEFAULT_TYPE
     elif data == 'eth_transfer':
         await query.message.reply_text("Please enter the recipient's ETH address:")
         context.user_data['waiting_for'] = 'eth_address'
+
 
 async def message_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if 'waiting_for' not in context.user_data:
@@ -151,7 +164,7 @@ async def message_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
         try:
             amount = float(update.message.text)
             telegram_id = update.message.from_user.id
-            
+
             response = requests.post(f'{BACKEND_URL}/transfer', json={
                 "telegram_id": str(telegram_id),
                 "toAddress": context.user_data['sol_address'],
@@ -159,7 +172,6 @@ async def message_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
                 "type": "SOL"
             })
 
-            
             parsed_response = json.loads(response.content)
             if response.status_code == 200:
                 await update.message.reply_text(
@@ -168,12 +180,12 @@ async def message_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
                 )
             else:
                 await update.message.reply_text(f"Transfer failed: {parsed_response['message']}")
-                
+
         except ValueError:
             await update.message.reply_text("Please enter a valid number for the amount")
         except Exception as e:
             await update.message.reply_text(f"An error occurred: {str(e)}")
-        
+
         context.user_data.clear()
         return
 
@@ -181,25 +193,27 @@ async def message_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
         try:
             amount = float(update.message.text)
             telegram_id = update.message.from_user.id
-            
+
             response = requests.post(f'{BACKEND_URL}/transfer', json={
                 "telegram_id": str(telegram_id),
                 "toAddress": context.user_data['eth_address'],
                 "amount": amount,
                 "type": "ETH"
             })
-            
             parsed_response = json.loads(response.content)
             if response.status_code == 200:
-                await update.message.reply_text(f"Transfer successful! Transaction hash: {parsed_response['hash']}")
+                await update.message.reply_text(
+                    text=f'Transaction hash: `{parsed_response['hash']}`',
+                    parse_mode='MarkdownV2'
+                )
             else:
                 await update.message.reply_text(f"Transfer failed: {parsed_response['message']}")
-                
+
         except ValueError:
             await update.message.reply_text("Please enter a valid number for the amount")
         except Exception as e:
             await update.message.reply_text(f"An error occurred: {str(e)}")
-        
+
         context.user_data.clear()
         return
 
@@ -207,5 +221,6 @@ if __name__ == "__main__":
     app = Application.builder().token(TOKEN).build()
     app.add_handler(CommandHandler('start', start))
     app.add_handler(CallbackQueryHandler(handle_button_click))
-    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, message_handler))
+    app.add_handler(MessageHandler(
+        filters.TEXT & ~filters.COMMAND, message_handler))
     app.run_polling()
